56d5f2b88d0cb64d903af59fd0ebd62207ed999e
rina0712mmm <rina0712.mmm@gmail.com>
2020-10-15 13:35:27 +0900

    Issue4〜6の解消（3回目）

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,6 +1,10 @@
-get_week
- days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: @wdays}
+  class CalendarsController < ApplicationController
 
+  # １週間のカレンダーと予定が表示されるページ
+  def index
+    get_week
+    @plan = Plan.new
+  end
 
   # 予定の保存
   def create
@@ -39,6 +43,5 @@ get_week
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
-
   end
 end
